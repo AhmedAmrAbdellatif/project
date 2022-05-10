6e827f1df39a1a29ed8532bc11bd2e42b2feca25
XAhmed-SalehX <105154498+XAhmed-SalehX@users.noreply.github.com>
2022-05-10 20:34:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -17,10 +17,15 @@ void main()
 	/////////		take inputs from Key bad and Disply it on LCD		/////////
 	for(i=0; i< 4; i++)
 	{
+		// SW 2 is pressed
+		if (Switch_Input (2) == 0) 
+		{
+			break;
+		}
 		time[0] = keypad_getkey; // new element is placed in t[o]
 		if (time[i] >= '0' && time[i] <= '9')	//time must be integer
 		{
-			//// 	print to LCD minutes		////
+			//// 	print to LCD minutes	////
 			for(j=3; j>1; j--)
 			{
 				LCD_data(time[j]); 
@@ -28,12 +33,12 @@ void main()
 			
 			LCD_data(" : "); 
 			
-			//// 	print to LCD seconds		////
+			//// 	print to LCD seconds	////
 			for(j=1; j>= 0; j--)
 			{
 				LCD_data(time[j]); 
 			}
-			////			shifting elements of time array			////
+			////		shifting elements of time array		////
 			for(j=i; j>=0&&(i+1<4); j--) //stop when you reach t[0] or t[4]
 			{
 					time[j+1] = time[j];	
@@ -42,10 +47,7 @@ void main()
 		// wrong input
 		else LCD_data("err"); 
 		
-		if (button_2 == 0) // SW 2 is pressed
-		{
-			break;
-		}
+		
 	
 	}
 	delays_display(calc(time));
