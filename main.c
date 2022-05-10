2bfe9f598a303296f0e84f0294dcd73168087aaf
XAhmed-SalehX <105154498+XAhmed-SalehX@users.noreply.github.com>
2022-05-10 20:10:45 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ unsigned long calc(char arr[4])
 }
 void main()
 {
-	char time[4] = { };	//	time array = 00 00
+	char time[4] = {'0', '0', '0', '0'};	//	time array = 00 00
 	long i, j; //	counters
 	
 	/////////		take inputs from Key bad and Disply it on LCD		/////////
