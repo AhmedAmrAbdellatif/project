1110bad05a49e6f8c6208c37e796786aca87172d
AhmedAmrAbdellatif <105156858+AhmedAmrAbdellatif@users.noreply.github.com>
2022-05-10 16:54:51 +0200

    Update initialization.c

--- initialization.c
+++ initialization.c
@@ -4,33 +4,33 @@ int counter;
 
 ////	main delay block	/////
 void SysTick_wait(unsigned long delay)												
-			{
-				NVIC_ST_CTRL_R = 0x0;
-				NVIC_ST_RELOAD_R = delay-1;
-				NVIC_ST_CURRENT_R = 0x0;
-				NVIC_ST_CTRL_R	|= 0x5;					
-				while( (NVIC_ST_CTRL_R & 0x00010000) == 0) {}				//Checking for count bit
-			}
+{
+	NVIC_ST_CTRL_R = 0x0;
+	NVIC_ST_RELOAD_R = delay-1;
+	NVIC_ST_CURRENT_R = 0x0;
+	NVIC_ST_CTRL_R	|= 0x5;					
+	while( (NVIC_ST_CTRL_R & 0x00010000) == 0) {}				//Checking for count bit
+}
 ////	delay 1 m Sec	/////		
 int delayms(unsigned long time)
-		{
-		for (counter=0; counter<time ; counter++)
-		{
-			SysTick_wait(16000);																																										
-		}
-		}
+{
+for (counter=0; counter<time ; counter++)
+{
+	SysTick_wait(16000);																																										
+}
+}
 		
 int delays_display(unsigned long time)												
-		{
-		for (counter=0; counter<time ; counter++)			//for(counter=time;counter>0;counter--)
-		{
-			LCD_data (counter);
-			delayms(1000);
-			sw3();//cheak if sw3 is pressed(door is open)
-			//sw1();cheak if sw1 is pressed
-		}
-		counter = 0;
-		}
+{
+for (counter=0; counter<time ; counter++)			//for(counter=time;counter>0;counter--)
+{
+	LCD_data (counter);
+	delayms(1000);
+	sw3();//cheak if sw3 is pressed(door is open)
+	//sw1();cheak if sw1 is pressed
+}
+counter = 0;
+}
 		
 void SystemInit(void){};
 void LCD_data(unsigned char data)
@@ -83,29 +83,29 @@ void LCD_init()
 }
 
 	void LEDSW_Init(void)
-				{
-						SYSCTL_RCGCGPIO_R	|= 0x20;							//PORT F Clock enable
-					
-						while ( (SYSCTL_PRGPIO_R&0x20) == 0) {}
-						
-						GPIO_PORTF_LOCK_R |= 0x4C4F434B;				//Unlock PORT F
-						GPIO_PORTF_CR_R |= 0x1F;								//Allow changes to pins 0,1,2,3,4
-						GPIO_PORTF_DIR_R |= 0x0E;								//SET pins 1,2,3 to be output
-						GPIO_PORTF_DIR_R &= ~0x11;							//SET pins 0,4 to be input
-						GPIO_PORTF_DEN_R |= 0x1F;								//SET pins 0,1,2,3,4 to be digital
-						GPIO_PORTF_AMSEL_R &= ~0x1F;						//Disable Analog function
-						GPIO_PORTF_AFSEL_R &= ~0x1F;						//No alternative function
-						GPIO_PORTF_PCTL_R &= ~0xFFFFF;					//GPIO clear bit PCTL
-						GPIO_PORTF_DATA_R &= ~0x0E;							//Initialize LEDs to be off
-						GPIO_PORTF_PUR_R |= 0x11;								//SET pull-up resistance
-				}
+{
+	SYSCTL_RCGCGPIO_R	|= 0x20;							//PORT F Clock enable
+
+	while ( (SYSCTL_PRGPIO_R&0x20) == 0) {}
+
+	GPIO_PORTF_LOCK_R |= 0x4C4F434B;				//Unlock PORT F
+	GPIO_PORTF_CR_R |= 0x1F;								//Allow changes to pins 0,1,2,3,4
+	GPIO_PORTF_DIR_R |= 0x0E;								//SET pins 1,2,3 to be output
+	GPIO_PORTF_DIR_R &= ~0x11;							//SET pins 0,4 to be input
+	GPIO_PORTF_DEN_R |= 0x1F;								//SET pins 0,1,2,3,4 to be digital
+	GPIO_PORTF_AMSEL_R &= ~0x1F;						//Disable Analog function
+	GPIO_PORTF_AFSEL_R &= ~0x1F;						//No alternative function
+	GPIO_PORTF_PCTL_R &= ~0xFFFFF;					//GPIO clear bit PCTL
+	GPIO_PORTF_DATA_R &= ~0x0E;							//Initialize LEDs to be off
+	GPIO_PORTF_PUR_R |= 0x11;								//SET pull-up resistance
+}
 
 /* Array of 4x4 to define characters which will be printe on specific key pressed */
 
 unsigned int ncols = 0;
 unsigned int nrows = 0;                                                                    
 unsigned const char symbol[4][4] = {{ '1', '2',  '3', 'A'},      
-																	 {  '4', '5',  '6', 'B'},      
+				   {  '4', '5',  '6', 'B'},      
                                    {  '7', '8',  '9', 'C'},      
                                    {  '*', '0',  '#', 'D'}}; 
 
@@ -132,23 +132,23 @@ and rows respectivley */
 char keypad_getkey(void)
 {
   while(1)
-  {
-				for(int i = 0; i < 4; i++)                       						 //scan columns
-					{
-					GPIO_PORTC_DATA_R = (1 << (i+4));
-					delayms (1);
-											for(int j = 0; j < 4; j++)                     //scan rows
-											{
-											if((GPIO_PORTE_DATA_R & 0x1E) & (1 << (j+1)))
-											return symbol[j][i];
-											}
-					}
-  }
+{
+for(int i = 0; i < 4; i++)                       						 //scan columns
+{
+GPIO_PORTC_DATA_R = (1 << (i+4));
+delayms (1);
+						for(int j = 0; j < 4; j++)                     //scan rows
+						{
+						if((GPIO_PORTE_DATA_R & 0x1E) & (1 << (j+1)))
+						return symbol[j][i];
+						}
+}
+}
 }				
 
 unsigned char SW1_Input (void)
-			{
-				
-						return GPIO_PORTF_DATA_R & 0x10;
-				
-			}
+{
+
+return GPIO_PORTF_DATA_R & 0x10;
+
+}
