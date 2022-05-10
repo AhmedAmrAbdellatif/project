65d6c144fd940c275cdb2a75b6c270219ae7f378
XAhmed-SalehX <105154498+XAhmed-SalehX@users.noreply.github.com>
2022-05-10 22:57:05 +0200

    Update and rename main.c to Push_D.c

---
+++ Push_D.c
@@ -0,0 +1,78 @@
+////	calculating cooking time in secends	////
+unsigned long calc(char arr[4])
+{
+    unsigned long duration = 0;
+    long i;
+    for (i = 0; i<4 ;i++)	// convert from char --> number
+    {
+        arr [i]= (arr[i] - '0');
+    }
+    duration = arr[0] + arr[1]*10 + arr[2]*60 + arr[3]*600 ;
+    return duration;
+}
+void print_time (char time[4])
+	{
+	long j; //	counters
+	//// 	display minutes on LCD 	////
+	for(j=3; j>1; j--)
+	{
+		LCD_data(time[j]); 
+	}
+
+	LCD_data(':'); 
+
+	//// 	display seconds on LCD 	////
+	for(j=1; j>= 0; j--)
+	{
+		LCD_data(time[j]); 
+	}
+}
+void Push_D()
+{
+	char time[4] = {'0', '0', '0', '0'};	//	time array = 00 : 00
+	long i, j; //	counters
+	
+	/////////		take inputs from Key bad and Disply it on LCD		/////////
+	Clear_LCD();		// clearing the LCD
+	LCD_data("Cooking Time?");
+	delayms(2000); 		// 2 sec delay
+	Clear_LCD();		// clearing the LCD
+	print_time(time);	// 00 : 00
+	for(i=0; i< 4; i++)
+	{
+		// SW 2 is pressed
+		if (Switch_Input (2) == 0) 
+		{
+			break;
+		}
+		time[0] = keypad_getkey; // new element is placed in t[o] --> (default : second)
+		if (time[i] >= '0' && time[i] <= '9')	//time must be number
+		{
+			print_time(time);
+			////	  shifting elements of time array	////
+			for(j=i; j>=0 && (i+1<4); j--) //stop when you reach t[0] or t[3]
+			{
+				time[j+1] = time[j];	// 10 minutes <--- 10 minutes <--- 10 Seconds <--- Seconds
+			}
+		}
+		// wrong input
+		else
+		{
+			Clear_LCD();
+			LCD_data("error");
+			delayms(2000);
+			Clear_LCD();
+			print_time(time);
+			i--;
+			
+			/*
+			handling the error :
+			
+			i = -1	: print error for 2 sec then take all inputs again 
+			or
+			i--	: print error for 2 sec then retake this input ( display the old inputs )
+			*/
+		}
+	}
+	delays_display(calc(time));
+}
