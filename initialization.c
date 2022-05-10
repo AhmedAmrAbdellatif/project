ad6074d393beb969f83dbf8f377d3ef2ac5c0547
Eslam Shaban Abdelmoneam <104197866+EslamShaban19@users.noreply.github.com>
2022-05-11 00:48:16 +0200

    Update initialization.c

--- initialization.c
+++ initialization.c
@@ -159,3 +159,16 @@ unsigned char Switch_Input (unsigned char i)
 			//return GPIO_PORTF_DATA_R & 0x01;
 			break;
 }
+void sw3_buzzer_init()
+{
+	SYSCTL_RCGCGPIO_R	|= 0x10;		//PORT E Clock enable
+	while ( (SYSCTL_PRGPIO_R&0x10) == 0) {}		
+	GPIO_PORTE_DIR_R &= ~0x01;			//SET pin 0 sw3 to be input 
+	GPIO_PORTE_DIR_R |= 0x20;			//SET pin 5 buzzer to be output
+	GPIO_PORTE_DEN_R |= 0x21;			//SET pins 0,5 to be digital
+	GPIO_PORTE_AMSEL_R &= ~0x21;			//Disable Analog function
+	GPIO_PORTE_AFSEL_R &= ~0x21;			//No alternative function
+	GPIO_PORTE_PCTL_R &= ~0xFFFFF;			//GPIO clear bit PCTL
+	GPIO_PORTE_DATA_R &= ~0x20;			//Initialize BUZZER to be off
+	GPIO_PORTF_PUR_R |= 0x01;			//SET pull-up resistance FOR sw3
+}
