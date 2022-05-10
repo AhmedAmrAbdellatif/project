f17700e3ec8d038f98bd4aa610187b7e36e7d122
Eslam Shaban Abdelmoneam <104197866+EslamShaban19@users.noreply.github.com>
2022-05-11 00:56:19 +0200

    Update RGB_Blink.c

--- RGB_Blink.c
+++ RGB_Blink.c
@@ -1,7 +1,7 @@
 void RGB_blinks (unsigned char LED){
-GPIO_PORTF_DATA_R &=~LED;   //clear leds
-delayms(500);
 GPIO_PORTF_DATA_R |=LED;    //set leds
+delayms(500);
+GPIO_PORTF_DATA_R &=~LED;   //clear leds
 delayms(500);
 }
 
