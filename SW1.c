ff945861d67cd9faf53db27df901f75aa6cefb6a
Ahmedtayel22 <105231666+Ahmedtayel22@users.noreply.github.com>
2022-05-12 00:08:44 +0200

    Update SW1.c

--- SW1.c
+++ SW1.c
@@ -2,10 +2,10 @@
 
 GPIO_PORTF_IS_R &= ~0x10;     // PF4 is edge-sensitive
 GPIO_PORTF_IBE_R &= ~0x10;   // PF4 is not both edges
-//GPIO_PORTF_IEV_R &= ~0x10;   // PF4 falling edge event  " Falling = switch down or switch up ?? "
+GPIO_PORTF_IEV_R &= ~0x10;   // PF4 falling edge event 
 GPIO_PORTF_ICR_R = 0x10;     // clear flag4
 GPIO_PORTF_IM_R |= 0x10;     // arm interrupt on PF4
-NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000;  // (g) priority 5 "can be changed"
+//NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000;  //(g) priority 5 "can be changed" - OPTIONAL
 NVIC_EN0_R = 0x40000000;     // enable interrupt 30 "Port F" in NVIC
 EnableInterrupts();         // Enable global Interrupt flag (I)
 
