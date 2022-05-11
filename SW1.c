b5eab324d8d27b73e7d777d9b4d53344acfd44ea
Ahmedtayel22 <105231666+Ahmedtayel22@users.noreply.github.com>
2022-05-12 01:18:04 +0200

    Update SW1.c

--- SW1.c
+++ SW1.c
@@ -1,5 +1,6 @@
+voltaile unsigned long FallingEdges = 0;  "global" //Acts like a counter. 
 //PortF Interrupt Initialization//
-
+FallingEdges = 0;             //After portF clock enable
 GPIO_PORTF_IS_R &= ~0x10;     // PF4 is edge-sensitive
 GPIO_PORTF_IBE_R &= ~0x10;   // PF4 is not both edges
 GPIO_PORTF_IEV_R &= ~0x10;   // PF4 falling edge event 
@@ -12,8 +13,7 @@ EnableInterrupts();         // Enable global Interrupt flag (I)
 
 //Function////InProgress//
 //IF SW1 pressed twice//
-
-voltaile unsigned long FallingEdges = 0;  "global" //Acts like a counter.  
+  
 void GPIOPortF_Handler(void){
  GPIO_PORTF_ICR_R = 0x10; // acknowledge flag4
 if(FallingEdges ==1 ){
