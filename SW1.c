638b3f7b5a0cd93acbcce5478b7b2a957ddb2e00
AhmedAmrAbdellatif <105156858+AhmedAmrAbdellatif@users.noreply.github.com>
2022-05-12 08:30:49 +0200

    Update SW1.c
    RisingEdges instead of FallingEdges

--- SW1.c
+++ SW1.c
@@ -1,9 +1,9 @@
-voltaile unsigned long FallingEdges = 0;  "global" //Acts like a counter. 
+voltaile unsigned long RisingEdges = 0;  "global" //Acts like a counter. 
 //PortF SW1-SW2 Interrupt Initialization//
 FallingEdges = 0;             //After portF clock enable
 GPIO_PORTF_IS_R &= ~0x11;     // PF4-0 is edge-sensitive
 GPIO_PORTF_IBE_R &= ~0x11;   // PF4-0 is not both edges
-GPIO_PORTF_IEV_R &= ~0x11;   // PF4-0 falling edge event 
+GPIO_PORTF_IEV_R |= 0x11;   // PF4-0 rising edge event 
 GPIO_PORTF_ICR_R = 0x11;     // clear flag4
 GPIO_PORTF_IM_R |= 0x11;     // arm interrupt on PF4-0
 //NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000;  //(g) priority 5 "can be changed" - OPTIONAL
@@ -17,14 +17,14 @@ void GPIOPortF_Handler(void){
   if(GPIO_PORTF_MIS_R & 0x10)   //check if interrupt causes by PF4/SW1
   {     
     GPIO_PORTF_ICR_R = 0x10;   // clear the interrupt flag 
-    if(FallingEdges ==1 )      //FallingEdges is initialized by '0'
+    if(RisingEdges ==1 )      //RisingEdges is initialized by '0'
     {
-       FallingEdges =0;        //clear the counter
+       RisingEdges =0;        //clear the counter
        stop();                
 		}
        else
        {
-         FallingEdges ++;     //FallingEdges=1
+         RisingEdges ++;     //RisingEdges=1
          pause();               //pause cooking
        }
 		 
@@ -32,7 +32,7 @@ void GPIOPortF_Handler(void){
    else if (GPIO_PORTF_MIS_R & 0x01)   //check if interrupt causes by PF0/SW2
    {
       GPIO_PORTF_ICR_R = 0x01;         // clear the interrupt flag 
-      if(FallingEdges ==1)              //to make sure that we are pasuing cooking
+      if(RisingEdges ==1)              //to make sure that we are pasuing cooking
 		 {
 			 resume();                       
 		 }
