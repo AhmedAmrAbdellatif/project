c276c15434fb5a01618a1bd2f89c663574c1ca6d
islam <105168467+Eng1slam@users.noreply.github.com>
2022-05-12 15:36:04 +0200

    Update SW2.c

--- SW2.c
+++ SW2.c
@@ -2,7 +2,7 @@
 
   void SW2_check_forcooking()
 {
-  while((GPIO_PORTF_DATA_R & 0x01) == 0x01){}    //Acting like a delay waiting the user to push SW2 to start cooking "go out from the funtion".
+  while((GPIO_PORTF_DATA_R & 0x01) == 0x01){}    //Acting like a delay waiting the user to push SW2 to start cooking lol "go out from the funtion".
 }
   
   //To start cooking after SW2 is pushed (=0x00), i suggest to check SW2 in " delays_display " function before go on the "for" loop.
