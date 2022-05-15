26e5d6750ab28ef6d1794a2abf0935f896552368
XAhmed-SalehX <105154498+XAhmed-SalehX@users.noreply.github.com>
2022-05-15 03:44:58 +0200

    Update state_D.c

--- state_D.c
+++ state_D.c
@@ -1,3 +1,15 @@
+////	calculating cooking time in secends	////
+unsigned long calc(char arr[4])
+{
+    unsigned long duration = 0;
+    long i;
+    for (i = 0; i<4 ;i++)	// convert from char --> number
+    {
+        arr [i]= (arr[i] - '0');
+    }
+    duration = arr[0] + arr[1]*10 + arr[2]*60 + arr[3]*600;
+    return duration;
+}
 void pushD()
 {
 	char time[4] = {'0', '0', '0', '0'};	//	time array = 00 : 00
