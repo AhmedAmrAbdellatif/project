10a7b8baefb01ce1dd73111df37895e9071425f3
Yossof Tayel <104010156+yossof-tayel@users.noreply.github.com>
2022-05-18 17:06:46 +0200

    Update Delay.h

--- Delay.h
+++ Delay.h
@@ -3,7 +3,7 @@ unsigned long counter_delay;
 ////	main delay block	/////
 void SysTick_wait(unsigned long delay)												
 {
-	NVIC_ST_CTRL_R = 0x00;
+	NVIC_ST_CTRL_R = 0x00;				//disable the systick
 	NVIC_ST_RELOAD_R = delay-1;
 	NVIC_ST_CURRENT_R = 0x00;
 	NVIC_ST_CTRL_R |= 0x05;
