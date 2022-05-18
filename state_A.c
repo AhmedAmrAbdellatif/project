89c152c2c0c5e33481fd3d67a56ad5be9322f31a
islam <105168467+Eng1slam@users.noreply.github.com>
2022-05-18 12:16:29 +0200

    Update state_A.c

--- state_A.c
+++ state_A.c
@@ -1,11 +1,14 @@
 void pushA()
       {
-	LCD_data('A');  //show 'A' on LCD
-	delayms(500);	//show 'A' on LCD for (0.5sec)
-	clear_LCD;
-	delayms(500);
-	LCD_string("Popcorn");
-	delayms(2000);
+	LCD_data('A');  	//show 'A' on LCD
+	delayms(500);		//show 'A' on LCD for (0.5sec)
+	clear_LCD;		//clear LCD
+	delayms(500);		//delay(0.5)
+	
+	/////////showing popcorn///////////
+	
+	LCD_string("Popcorn");	//show 'A' on LCD
+	delayms(2000);		//show 'A' on LCD for (2sec)
 	clear_LCD;
 
 	LCD_countdown_display(60);
