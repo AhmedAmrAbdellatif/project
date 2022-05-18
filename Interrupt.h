f1394b0152b080c844b5f547b6c857f0c045543e
Ahmedtayel22 <105231666+Ahmedtayel22@users.noreply.github.com>
2022-05-18 08:42:13 +0200

    Update Interrupt.h

--- Interrupt.h
+++ Interrupt.h
@@ -1,1 +1,28 @@
+void Interrupt_Init()          
+	{
+	SYSCTL_RCGCGPIO_R	|= 0x10;		//PORT E Clock enable
+
+	while ( (SYSCTL_PRGPIO_R&0x10) == 0) {}	
+
+		GPIO_PORTE_IS_R |= 0x1;      //PE0 is edge-sensitive
+		GPIO_PORTE_IBE_R |= 0x0;     //PE0 is not both edges
+		GPIO_PORTE_IEV_R |= 0x1;     //PE0 falling edge event
+		GPIO_PORTE_ICR_R = 0x1; 
+		GPIO_PORTE_IM_R |= 0x1;    
+		
+	NVIC_PRI1_R = (NVIC_PRI1_R&0xFFFFFF00)|0x00000040;
+	NVIC_EN0_R = 0x10;    
+		
+	EnableInterrupts();        //Enable global Interrupts
+	
+	}
+
+
+	void GPIOPortE_Handler(void)
+		{
+					GPIO_PORTE_ICR_R = 0x1;
+					blink();
+		}
+	
+
 
