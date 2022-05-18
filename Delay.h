32e39f5f9a8a7478c11e8a26fa37bf7cec03e696
Yossof Tayel <104010156+yossof-tayel@users.noreply.github.com>
2022-05-18 17:22:21 +0200

    Update Delay.h

--- Delay.h
+++ Delay.h
@@ -3,10 +3,10 @@ unsigned long counter_delay;
 /////     main delay block     /////
 void SysTick_wait(unsigned long delay)												
 {
-	NVIC_ST_CTRL_R = 0x00;				//disable the systick
+	NVIC_ST_CTRL_R = 0x00;				//disabling the systick
 	NVIC_ST_RELOAD_R = delay-1;			//entering the counting value
 	NVIC_ST_CURRENT_R = 0x00;			//clearing the current value
-	NVIC_ST_CTRL_R |= 0x05;				//enable the systick & setting clock source bit 
+	NVIC_ST_CTRL_R |= 0x05;				//enabling the systick & setting clock source bit 
 	while( (NVIC_ST_CTRL_R & 0x00010000) == 0) {}	//checking for count bit
 }
 
