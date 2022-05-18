811e72e12b547290e1fa40e79cb5a4c1ee0bd0ae
Yossof Tayel <104010156+yossof-tayel@users.noreply.github.com>
2022-05-18 18:02:55 +0200

    Update LCD_Init.h

--- LCD_Init.h
+++ LCD_Init.h
@@ -8,13 +8,14 @@ void LCD_data(unsigned char data)
 	GPIO_PORTA_DATA_R &= ~0x80;		//clearing EN
 	delayms(2);				//delay 2 milliseconds
 }
+
 void LCD_command(unsigned char command)
 {
 	GPIO_PORTA_DATA_R &= ~0x20; 		//clearing RS
 	delayms(2);				//delay 2 milliseconds
-	GPIO_PORTA_DATA_R |= 0x80; 		// EN = 1
+	GPIO_PORTA_DATA_R |= 0x80; 		//setting EN
 	GPIO_PORTB_DATA_R = command;		//sending command
 	delayms(2);				//delay 2 milliseconds
-	GPIO_PORTA_DATA_R &= ~0x80; 		// EN = 0
+	GPIO_PORTA_DATA_R &= ~0x80; 		//clearing EN
 	delayms(2);				//delay 2 milliseconds
 }
