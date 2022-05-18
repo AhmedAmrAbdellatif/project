fda72331f499b7d6e849c90b0ae630ed491135aa
Yossof Tayel <104010156+yossof-tayel@users.noreply.github.com>
2022-05-18 17:10:13 +0200

    Update Delay.h

--- Delay.h
+++ Delay.h
@@ -7,7 +7,7 @@ void SysTick_wait(unsigned long delay)
 	NVIC_ST_RELOAD_R = delay-1;			//entering the counting value
 	NVIC_ST_CURRENT_R = 0x00;			//clearing the current value
 	NVIC_ST_CTRL_R |= 0x05;				//enable the systick & setting clock source bit 
-	while( (NVIC_ST_CTRL_R & 0x00010000) == 0) {}	
+	while( (NVIC_ST_CTRL_R & 0x00010000) == 0) {}	//checking for count bit
 }
 
 ////	delay 1 m Sec	/////
