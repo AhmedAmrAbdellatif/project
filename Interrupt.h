ddf59ed4746fb887900c61a8a215d0a1e6597366
Ahmedtayel22 <105231666+Ahmedtayel22@users.noreply.github.com>
2022-05-18 08:57:11 +0200

    Update Interrupt.h

--- Interrupt.h
+++ Interrupt.h
@@ -7,7 +7,7 @@ void Interrupt_Init()
 		GPIO_PORTE_IS_R |= 0x1;      //PE0 is edge-sensitive
 		GPIO_PORTE_IBE_R |= 0x0;     //PE0 is not both edges
 		GPIO_PORTE_IEV_R |= 0x1;     //PE0 falling edge event
-		GPIO_PORTE_ICR_R = 0x1;      //clear flag
+		GPIO_PORTE_ICR_R = 0x1;      //clear flag0
 		GPIO_PORTE_IM_R |= 0x1;      //arm interrupt on PE0
 		
 	NVIC_PRI1_R = (NVIC_PRI1_R&0xFFFFFF00)|0x00000040;  
@@ -20,7 +20,7 @@ void Interrupt_Init()
 
 	void GPIOPortE_Handler(void)
 		{
-					GPIO_PORTE_ICR_R = 0x1;   // acknowledge flag
+					GPIO_PORTE_ICR_R = 0x1;   // acknowledge flag0
 					blink();
 		}
 	
