bece179899bd433f6553bad6644fa5ede898cf91
XAhmed-SalehX <105154498+XAhmed-SalehX@users.noreply.github.com>
2022-05-18 13:26:21 +0200

    Update state_D.c

--- state_D.c
+++ state_D.c
@@ -10,3 +10,21 @@ unsigned long calc(char arr[4])
     duration = arr[0] + arr[1]*10 + arr[2]*60 + arr[3]*600;
     return duration;
 }
+
+void print_time (char time[4])
+{
+    long j; //	counter
+    //// 	display minutes on LCD 	////
+	for(j=3; j>1; j--)
+	{
+		LCD_data(time[j]); 
+	}
+
+	LCD_data(':'); 
+	
+	//// 	display seconds on LCD 	////
+	for(j=1; j>= 0; j--)
+	{
+		LCD_data(time[j]); 
+	}
+}
