9d915db3e0f8d5ea3d8457360b49daf07e8ce2f3
Eslam Shaban Abdelmoneam <104197866+EslamShaban19@users.noreply.github.com>
2022-05-18 21:44:34 +0200

    Update state_B.c

--- state_B.c
+++ state_B.c
@@ -2,22 +2,22 @@
 void pushB()
 		{
 		char temp_input;	
-		LCD_data('B');                          // print "B" on LCD for 1 sec
+		LCD_data('B');                                // print "B" on LCD for 1 sec
 		delayms(1000);                   
-		clear_LCD;                              // clear LCD 
+		clear_LCD;                                   // clear LCD 
 		delayms(50);
-		question:LCD_string("Beef Weight?");    // print "Beaf weight?" on LCD for 50 ms 
+		question:LCD_string("Beef Weight?");         // print "Beaf weight?" on LCD for 50 ms 
 		delayms(50);
 		
-		temp_input = keypad_input();            // get input from keypad
+		temp_input = keypad_input();                 // get input from keypad
 		LCD_data(temp_input);	
 			
 			
-		if ( temp_input <='9' && temp_input >='1')
+		if ( temp_input <='9' && temp_input >='1')  // check if 1<=input<=9   
 		{
 			
 
-					while (sw2() != 0x00)
+					while (sw2() != 0x00)    // if SW1 is pressed clear LCD in condition not pressing SW2
 					{
 						if (sw1() == 0x00)
 						{
@@ -27,7 +27,7 @@ void pushB()
 						}
 					}
 					
-				delayms(20);
+				delayms(20);                    // SW2 is pressed start operation 
 				clear_LCD;
 				desired_weight('B', temp_input );		
 
