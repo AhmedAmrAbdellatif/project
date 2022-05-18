0990ae62532d5710f5e6dc18255cbfc249bdf38b
Eslam Shaban Abdelmoneam <104197866+EslamShaban19@users.noreply.github.com>
2022-05-18 09:07:43 +0200

    Update Switches.h

--- Switches.h
+++ Switches.h
@@ -10,5 +10,23 @@ void sw3_buzzer_init()
 	GPIO_PORTE_PCTL_R &= ~0xFFFFF;			//GPIO clear bit PCTL
 	GPIO_PORTE_DATA_R &= ~0x20;			//Initialize BUZZER to be off
 	GPIO_PORTE_PDR_R |= 0x01;			//SET pull-down resistance FOR sw3   0 is open door , 1 is closed door
+}  
+void LEDSW_Init()
+{
+	SYSCTL_RCGCGPIO_R	|= 0x20;		//PORT F Clock enable
+
+	while ( (SYSCTL_PRGPIO_R&0x20) == 0) {}		
+
+	GPIO_PORTF_LOCK_R |= 0x4C4F434B;		//Unlock PORT F
+	GPIO_PORTF_CR_R |= 0x1F;			//Allow changes to pins 0,1,2,3,4
+	GPIO_PORTF_DIR_R |= 0x0E;			//SET pins 1,2,3 to be output
+	GPIO_PORTF_DIR_R &= ~0x11;			//SET pins 0,4 to be input
+	GPIO_PORTF_DEN_R |= 0x1F;			//SET pins 0,1,2,3,4 to be digital
+	GPIO_PORTF_AMSEL_R &= ~0x1F;			//Disable Analog function
+	GPIO_PORTF_AFSEL_R &= ~0x1F;			//No alternative function
+	GPIO_PORTF_PCTL_R &= ~0xFFFFF;			//GPIO clear bit PCTL
+	GPIO_PORTF_DATA_R &= ~0x0E;			//Initialize LEDs to be off
+	GPIO_PORTF_PUR_R |= 0x11;			//SET pull-up resistance
 }
+
 
