7ddbbff9a7a5b722de9205b25e94034504455e3d
AhmedAmrAbdellatif <105156858+AhmedAmrAbdellatif@users.noreply.github.com>
2022-05-18 09:00:36 +0200

    Update state_B.c

--- state_B.c
+++ state_B.c
@@ -1,1 +1,47 @@
 
+void pushB()
+		{
+		char temp_input;	
+		LCD_data('B');
+		delayms(1000);
+		clear_LCD;
+		delayms(50);
+		question:LCD_string("Beef Weight?");
+		delayms(50);
+		
+		temp_input = keypad_input();
+		LCD_data(temp_input);	
+			
+			
+		if ( temp_input <='9' && temp_input >='1')
+		{
+			
+
+					while (sw2() != 0x00)
+					{
+						if (sw1() == 0x00)
+						{
+							clear_LCD;
+							delayms(50);
+							goto question;
+						}
+					}
+					
+				delayms(20);
+				clear_LCD;
+				desired_weight('B', temp_input );		
+
+		}
+		
+		else
+			{
+				clear_LCD;
+				LCD_string("Invalid Input");
+				delayms(1500);
+				clear_LCD;
+				LCD_string("Only Between 1:9");
+				delayms(1500);
+				clear_LCD;
+				goto question;
+			}				
+		}
