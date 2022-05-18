1e681f8bb151ab46fa5edc1c3f3b79abc2db219d
Yossof Tayel <104010156+yossof-tayel@users.noreply.github.com>
2022-05-18 17:31:52 +0200

    Update LCD_Init.h

--- LCD_Init.h
+++ LCD_Init.h
@@ -1,1 +1,11 @@
-
+void LCD_data(unsigned char data)
+{
+	GPIO_PORTA_DATA_R &= ~0x40;
+	GPIO_PORTA_DATA_R |= 0x20;
+	delayms(2);
+	GPIO_PORTA_DATA_R |= 0x80;
+	GPIO_PORTB_DATA_R = data;
+	delayms(2);
+	GPIO_PORTA_DATA_R &= ~0x80;
+	delayms(2);
+}
