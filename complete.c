47e3ce450fee9a02a93cc72dd8e4dfb48f1626c6
Eslam Shaban Abdelmoneam <104197866+EslamShaban19@users.noreply.github.com>
2022-05-18 09:18:50 +0200

    Update complete.c

--- complete.c
+++ complete.c
@@ -2,4 +2,17 @@ void complete()
 {				
 				clear_LCD;
 				delayms(1);
-				
+				LCD_data('0');
+				LCD_data('0');
+				LCD_data(':');
+				LCD_data('0');
+				LCD_data('0');
+	
+				blink_buzzer(3);
+	
+				BUZZER_OFF; //clear buzzer pin
+				LED_OFF; //clear leds	
+				delayms(100);
+				clear_LCD;
+	
+}				
