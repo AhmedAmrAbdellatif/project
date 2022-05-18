ba7a7fab79a51240230e14acb55d09d3f2d04576
Yossof Tayel <104010156+yossof-tayel@users.noreply.github.com>
2022-05-18 13:52:55 +0200

    Update Delay.h

--- Delay.h
+++ Delay.h
@@ -2,9 +2,10 @@ unsigned long counter_delay;
 
 void SysTick_wait(unsigned long delay)												
 {
-	NVIC_ST_CTRL_R = 0x0;
+	NVIC_ST_CTRL_R = 0x00;
 	NVIC_ST_RELOAD_R = delay-1;
-	NVIC_ST_CURRENT_R = 0x0;
+	NVIC_ST_CURRENT_R = 0x00;
+	NVIC_ST_CTRL_R |= 0x05;
 	while( (NVIC_ST_CTRL_R & 0x00010000) == 0) {
 	}
 }
