a0cd35391ba8fb8997ddd3ce6f1d6d1df4a1d5b8
Yossof Tayel <104010156+yossof-tayel@users.noreply.github.com>
2022-05-18 17:37:38 +0200

    Update LCD_Init.h

--- LCD_Init.h
+++ LCD_Init.h
@@ -1,6 +1,5 @@
 void LCD_data(unsigned char data)
 {
-	//GPIO_PORTA_DATA_R &= ~0x40;		// R/W will be connected to the GND externally
 	GPIO_PORTA_DATA_R |= 0x20;		// RS = 1
 	delayms(2);
 	GPIO_PORTA_DATA_R |= 0x80;		// EN = 1
